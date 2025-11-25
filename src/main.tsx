5eec5ef1718e638ac754171909edf9a025b1217e
DESKTOP-FAPRJQS\hg <han970112@gmail.com>
2025-11-26 07:21:51 +0900

    nginx conf 프록시헤더 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App';
 import { validateEnv } from './config/env';
 import '../index.css';
 
+console.log('VITE_N8N_CHECKLIST_WEBHOOK_URL:', import.meta.env.VITE_N8N_CHECKLIST_WEBHOOK_URL);
+
 // 환경변수 검증 (개발 모드에서만 경고)
 if (import.meta.env.DEV) {
   validateEnv();
